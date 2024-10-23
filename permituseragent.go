a5f3d467692df9de4419239cac6dfcbeaf3dcb7a
ret2binsh <ret2binsh@binarychop.shop>
2024-10-23 13:21:11 -0400

    Fixed logging
    After research, found that someone found out that to print debug
    messages in middleware plugins use fmt.Print and for err use log.Print.
    Other logging levels required additional implementations but is not
    necessary for this project. Reference this for future needs
    https://github.com/taskmedia/ddns-allowlist/blob/main/pkg/log/logger.go

--- permituseragent.go
+++ permituseragent.go
@@ -5,9 +5,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-
-	"github.com/rs/zerolog"
-	"github.com/traefik/traefik/v3/pkg/middlewares"
 )
 
 const typeName = "PermitUserAgent"
@@ -37,14 +34,13 @@ type permitUserAgent struct {
 // New creates and returns a plugin instance.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 
-	logger := middlewares.GetLogger(ctx, name, typeName)
-	logger.Debug().Msg("Creating middleware")
+	fmt.Println("Creating middleware")
 
 	if config.UserAgent == "" {
 		return nil, fmt.Errorf(name, " must have UserAgent value set.")
 	}
 
-	logger.Debug().Msgf("Loading %s plugin with settings: url -> %s useragent -> %s", name, config.Url, config.UserAgent)
+	fmt.Printf("Loading %s plugin with settings: url -> %s useragent -> %s", name, config.Url, config.UserAgent)
 
 	return &permitUserAgent{
 		name:    name,
@@ -57,18 +53,18 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 
 func (p *permitUserAgent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if req != nil {
-		p.logger.Debug().Msgf("Checking UserAgent for connection from: %s", req.RemoteAddr)
+		fmt.Printf("Checking UserAgent for connection from: %s", req.RemoteAddr)
 		userAgent := req.UserAgent()
 
 		// if the request useragent doesn't match our defined useragent
 		// then redirect to the defined url
 		if userAgent != p.userAgent {
-			p.logger.Warn().Msgf("Redirecting User-Agent: '%s' to URL: '%s'", userAgent, p.url)
+			log.Printf("Redirecting User-Agent: '%s' to URL: '%s'", userAgent, p.url)
 			rw.Header().Set("Location", p.url)
 			rw.WriteHeader(http.StatusFound)
 			return
 		} else {
-			p.logger.Debug().Msgf("Successful UserAgent match: %s", userAgent)
+			fmt.Printf("Successful UserAgent match: %s", userAgent)
 		}
 	}
 
