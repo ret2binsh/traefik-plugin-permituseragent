808da03db3c158b59f95f20466f67a928c3453c4
ret2binsh <ret2binsh@binarychop.shop>
2024-10-23 10:06:14 -0400

    Updated logger and added go vendor
    Updated the logger to use the traefik middlewares logger. This required
    additional dependencies and traefik plugins require the vendor folder
    for external dependencies.

--- go.mod
+++ go.mod
@@ -1,3 +1,33 @@
 module github.com/ret2binsh/traefik-plugin-permituseragent
 
-go 1.21.0
+go 1.23.0
+
+toolchain go1.23.2
+
+require (
+	github.com/rs/zerolog v1.33.0
+	github.com/traefik/traefik/v3 v3.1.6
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.44.327 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-acme/lego/v4 v4.18.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.2 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/http-wasm/http-wasm-host-go v0.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/miekg/dns v1.1.59 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/traefik/paerser v0.2.1 // indirect
+	github.com/vulcand/oxy/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/tools v0.25.0 // indirect
+)
--- permituseragent.go
+++ permituseragent.go
@@ -4,9 +4,13 @@ package traefik_plugin_permituseragent
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/traefik/traefik/v3/pkg/middlewares"
 )
+
+const typeName = "PermitUserAgent"
 
 // Config holds the plugin configuration.
 type Config struct {
@@ -27,39 +31,44 @@ type permitUserAgent struct {
 	next    http.Handler
 	userAgent string
 	url string
+	logger *zerolog.Logger
 }
 
 // New creates and returns a plugin instance.
-func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+
+	logger := middlewares.GetLogger(ctx, name, typeName)
+	logger.Debug().Msg("Creating middleware")
 
 	if config.UserAgent == "" {
 		return nil, fmt.Errorf(name, " must have UserAgent value set.")
 	}
 
-	log.Printf("Loading %s plugin with settings: url -> %s useragent -> %s", name, config.Url, config.UserAgent)
+	logger.Debug().Msgf("Loading %s plugin with settings: url -> %s useragent -> %s", name, config.Url, config.UserAgent)
 
 	return &permitUserAgent{
 		name:    name,
 		next:    next,
 		userAgent: config.UserAgent,
 		url: config.Url,
+		logger: logger,
 	}, nil
 }
 
 func (p *permitUserAgent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if req != nil {
-		log.Printf("Checking UserAgent for connection from: %s", req.RemoteAddr)
+		p.logger.Debug().Msgf("Checking UserAgent for connection from: %s", req.RemoteAddr)
 		userAgent := req.UserAgent()
 
 		// if the request useragent doesn't match our defined useragent
 		// then redirect to the defined url
 		if userAgent != p.userAgent {
-			log.Printf("Redirecting User-Agent: '%s'", userAgent)
+			p.logger.Warn().Msgf("Redirecting User-Agent: '%s' to URL: '%s'", userAgent, p.url)
 			rw.Header().Set("Location", p.url)
 			rw.WriteHeader(http.StatusFound)
 			return
 		} else {
-			log.Printf("Successful UserAgent match: %s", userAgent)
+			p.logger.Debug().Msgf("Successful UserAgent match: %s", userAgent)
 		}
 	}
 
